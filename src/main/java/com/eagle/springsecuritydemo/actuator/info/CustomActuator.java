74adba13734579c0703745ff2bb349bacad40665
EagleBladez <34265042+EagleBladez@users.noreply.github.com>
2022-04-13 16:13:07 +0630

    Update CustomActuator.java
    Test jenkins

--- src/main/java/com/eagle/springsecuritydemo/actuator/info/CustomActuator.java
+++ src/main/java/com/eagle/springsecuritydemo/actuator/info/CustomActuator.java
@@ -16,7 +16,7 @@ public class CustomActuator {
 	public Map<String, String> custom(final String desc){
 		Map<String, String> detail = new HashMap<String, String>();
 		detail.put("name", "who");
-		detail.put("phno", "+9993939");
+		detail.put("phno", "+11111");
 		detail.put("desc", desc);
 		return detail;
 	}
